8dce84f0b8a1f58e0147f6e9163d9599b12bb807
jackson211 <jackson21182@gmail.com>
2018-04-23 12:12:56 +0800

    clean

--- batch_process.sh
+++ batch_process.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-BASE="/Users/agoraqa/Desktop/2.2主版本/lag_videos/iphone7/edit"
+BASE="/Users/agoraqa/Desktop/lag/edit"
 FILES="${BASE}/*.mov"
 
 for f in $FILES
 do
   dname=${f%.mov}.csv
-  # result_name=${f%.mov}_result.csv
   echo "Processing $f file..."
-  python3 gaussian.py -i $f -o $dname
-  # echo "Cleaning $dname file..."
-  # python3 data_filter.py -i $dname -o $result_name
-  # take action on each file. $f store current file name
-  # echo "Saved to $result_name."
+  python3 gaussian.py -i $f
 done
